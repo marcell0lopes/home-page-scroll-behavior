2749a5d5c00d4ff14612fffa4fa7f7820eadca64
Marcello Lopes <marcell0lopes@outlook.com>
2022-01-29 14:56:16 -0300

    models behavior change

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -4,26 +4,57 @@ import UniqueOverlay from '../UniqueOverlay';
 import { Container, Spacer } from './styles';
 
 export function Page() {
+  const models = [
+    {
+      name: 'Mechanical Keyboards',
+      description: 'Scroll down to see more',
+      buttonText: 'Buy now',
+    },
+    {
+      name: 'Insane Keycaps',
+      description: 'Thousands of styles to choose',
+      buttonText: 'More Keycaps',
+    },
+    {
+      name: 'The Best Switches',
+      description: 'Choose if you want better typing or gaming',
+      buttonText: 'More Switches',
+    },
+    {
+      name: 'Strong Hardware',
+      description: 'Durable, resistent and perfect-weighted',
+      buttonText: 'Our Hardware',
+    },
+    {
+      name: 'Choose your fighter',
+      description: 'You can customize anything you want',
+      buttonText: 'custom keyboards',
+    },
+    {
+      name: 'May the LED be with you',
+      description: 'Choose how much your keyboard is going to shine',
+      buttonText: 'led options',
+    },
+    {
+      name: 'Support me!',
+      description: 'Check out my links below',
+      buttonText: 'links below',
+    },
+  ];
+
   return (
     <Container>
       <ModelsWrapper>
-        {[
-          'Model One',
-          'Model Two',
-          'Model Three',
-          'Model Four',
-          'Model Five',
-          'Model Six',
-          'Model Seven',
-        ].map((modelName) => (
+        {models.map((model) => (
           <ModelSection
-            key={modelName}
+            key={model.name}
             className="colored"
-            modelName={modelName}
+            modelName={model.name}
             overlayNode={
               <DefaultOverlayContent
-                label={modelName}
-                description="Order Online for Delivery"
+                label={model.name}
+                description={model.description}
+                buttonText={model.buttonText}
               />
             }
           />
